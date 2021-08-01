657e76ca780c6e59679e3268ac7759dcc09a567b
noor4apps <noor4apps@gmail.com>
2021-08-02 02:37:04 +0300

    #01 add routes dashboard\web

---
+++ routes/dashboard/web.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::prefix('dashboard')->name('dashboard.')->group(function () {
+
+    Route::get('/check', function () {
+        return "This is dashboard";
+    });
+
+});// end of dashboard routes
