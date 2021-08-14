5c265bc22240ac3eaeaf0159f1351131d88dee53
noor4apps <noor4apps@gmail.com>
2021-08-14 23:37:58 +0300

    #15

--- app/Http/Controllers/Dashboard/CategoryController.php
+++ app/Http/Controllers/Dashboard/CategoryController.php
@@ -8,9 +8,20 @@ use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
-    public function index()
+    public function __construct()
     {
-        $categories = Category::paginate(5);
+        $this->middleware(['permission:read_categories'])->only(['index']);
+        $this->middleware(['permission:create_categories'])->only(['create', 'store']);
+        $this->middleware(['permission:update_categories'])->only(['edit', 'update']);
+        $this->middleware(['permission:delete_categories'])->only(['destroy']);
+    }// end of construct
+
+    public function index(Request $request)
+    {
+        $categories = Category::when($request->search, function ($query) use ($request) {
+            return $query->where('name', 'like', '%' . $request->search . '%');
+        })->latest()->paginate(5);
+
         return view('dashboard.categories.index', compact('categories'));
     }// end of index
 
@@ -18,5 +29,40 @@ class CategoryController extends Controller
     {
         return view('dashboard.categories.create');
     }// end of create
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|unique:categories'
+        ]);
+
+        Category::create($request->all());
+
+        return redirect()->route('dashboard.categories.index')->with('success', __('site.added_successfully'));
+    }// end of store
+
+    public function edit(Category $category)
+    {
+        return view('dashboard.categories.edit', compact('category'));
+    }// end of edit
+
+    public function update(Request $request, Category $category)
+    {
+        $request->validate([
+            'name' => 'required|unique:categories,name,' . $category->id
+        ]);
+
+        $category->update($request->all());
+
+        return redirect()->route('dashboard.categories.index')->with('success', __('site.updated_successfully'));
+
+    }// end of update
+
+    public function destroy(Category $category)
+    {
+        $category->delete();
+
+        return redirect()->route('dashboard.categories.index')->with('success', __('site.deleted_successfully'));
+    }// end of destroy
 
 }// end of controller
--- resources/views/dashboard/categories/edit.blade.php
+++ resources/views/dashboard/categories/edit.blade.php
@@ -7,9 +7,9 @@
         <section class="content-header">
             <h1>@lang('site.categories')</h1>
             <ol class="breadcrumb">
-                <li><a href="{{ route('dashboard.index') }}"><i class="fa fa-dashboard"></i> @lang('site.dashboard')</li></a>
-                <li><a href="{{ route('dashboard.categories.index') }}"> @lang('site.categories')</li></a>
-                <li class="active"> @lang('site.add')</li></a>
+                <li><a href="{{ route('dashboard.index') }}"><i class="fa fa-dashboard"></i> @lang('site.dashboard')</a></li>
+                <li><a href="{{ route('dashboard.categories.index') }}"> @lang('site.categories')</a></li>
+                <li class="active"> @lang('site.edit')</li>
             </ol>
         </section>
 
@@ -17,18 +17,19 @@
         <section class="content">
             <div class="box box-primary">
                 <div class="box-header with-border">
-                    <h3 class="box-title">@lang('site.add')</h3>
+                    <h3 class="box-title">@lang('site.edit')</h3>
                 </div><!-- end of box header -->
                 <div class="box-body">
-                    <form action="{{ route('dashboard.categories.store') }}" method="post">
+                    <form action="{{ route('dashboard.categories.update', $category->id) }}" method="post">
                         @csrf
+                        @method('put')
                         <div class="form-group">
                             <label for="name">@lang('site.name')</label>
-                            <input type="text" name="name" id="name" class="form-control" value="{{ old('name') }}">
+                            <input type="text" name="name" id="name" class="form-control" value="{{ old('name', $category->name) }}">
                             @error('name')<small class="text-danger">{{ $message }}</small>@enderror
                         </div>
                         <div class="form-group">
-                            <button type="submit" class="btn btn-primary"><i class="fa fa-save"></i> @lang('site.save')</button>
+                            <button type="submit" class="btn btn-primary"><i class="fa fa-edit"></i> @lang('site.edit')</button>
                         </div>
                     </form> <!-- end of form -->
                 </div><!-- end of box body -->
